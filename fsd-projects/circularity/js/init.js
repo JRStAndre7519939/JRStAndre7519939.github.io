cfae152c7582bdc2eaa1901717717a8dba693f3c
JRStAndre7519939 <js7519939@lpssonline.com>
2026-03-30 15:13:16 +0000

    todo 7 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -52,6 +52,12 @@ drawCircle()
 
 
         // TODO 7 : Use a loop to create multiple circles
+for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
+
+            drawCircle();
+
+        }
+
 
 
 
